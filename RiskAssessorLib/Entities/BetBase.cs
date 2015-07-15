f5a3e8967542c70fb3ead1682d078c4fe2902a06
Christina-Nicola <christina.nicola@fairfaxmedia.com.au>
2015-07-15 18:22:05 +1000

    Added entities implementation

---
+++ RiskAssessorLib/Entities/BetBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskAssessorLib.Entities
+{
+    internal abstract class BetBase: IBet
+    {
+        public int BetId { get; set; }
+
+        public int ParticipantId { get; set; }       
+
+        public int EventId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public double Stake { get; set; }
+    }
+}
---
+++ RiskAssessorLib/Entities/SettledBet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskAssessorLib.Entities
+{
+    internal class SettledBet:BetBase,ISettledBet
+    {
+        public double AmountWon { get; set; }
+    }
+}
