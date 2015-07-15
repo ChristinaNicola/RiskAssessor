816a45b816f8f4c2184c032240daf838f764d3cb
Christina-Nicola <christina.nicola@fairfaxmedia.com.au>
2015-07-15 18:03:36 +1000

    Initial solution with main entity interfaces defined

---
+++ RiskAssessorLib/Entities/IBet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskAssessorLib.Entities
+{
+    public interface IBet
+    {
+        int BetId { get; }
+        int ParticipantId { get; }
+        int EventId { get; }
+        int CustomerId { get; }
+        double Stake { get; }
+    }
+}
